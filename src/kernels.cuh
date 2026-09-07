dfd741a5903eafae5299f9b1281ad696ae01785d
Aditya <aditya@Adityas-MacBook-Pro-3.local>
2026-09-07 21:52:08 +0530

    Disable kernel 12 for P100

--- src/kernels.cuh
+++ src/kernels.cuh
@@ -2,7 +2,7 @@
 
 #include "kernels/10_kernel_warptiling.cuh"
 #include "kernels/11_kernel_double_buffering.cuh"
-#include "kernels/12_kernel_double_buffering.cuh"
+// #include "kernels/12_kernel_double_buffering.cuh"
 #include "kernels/1_naive.cuh"
 #include "kernels/2_kernel_global_mem_coalesce.cuh"
 #include "kernels/3_kernel_shared_mem_blocking.cuh"
@@ -11,4 +11,4 @@
 #include "kernels/6_kernel_vectorize.cuh"
 #include "kernels/7_kernel_resolve_bank_conflicts.cuh"
 #include "kernels/8_kernel_bank_extra_col.cuh"
-// #include "kernels/9_kernel_autotuned.cuh"
+#include "kernels/9_kernel_autotuned.cuh"
